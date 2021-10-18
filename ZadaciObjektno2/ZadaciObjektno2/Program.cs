c4f8b1e248cf33268d016be0caf2e3a1c5cb1517
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-10-18 09:25:33 +0200

    Add files via upload

---
+++ ZadaciObjektno2/ZadaciObjektno2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciObjektno2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+            Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+            Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ ZadaciObjektno2/ZadaciObjektno2/Zrakoplov.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciObjektno2
+{
+    class Zrakoplov
+    {
+        private string Naziv; 
+        private double SnagaMotora; 
+        private int DosegLeta; 
+        public Zrakoplov(string s, double d, int i) 
+        { 
+            Naziv = s; 
+            SnagaMotora = d; 
+            DosegLeta = i; 
+        }
+    }
+}
+
